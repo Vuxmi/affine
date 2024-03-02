6ba26fe39e4f712bc8c7bec7123c7d6e2ca85f3b
minhvu <minhvunguyen2000@gmail.com>
2024-03-02 21:52:00 +0700

    PROJECT CREATED

---
+++ affine.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+#include <string.h>
+
+#include <ctype.h>
+
+// Define keys
+
+int a = 5;
+
+int b = 7;
+
+
+char encryptChar(char ch) 
+{
+    if (isalpha(ch)) 
+    {
+
+    char base = isupper(ch) ? 'A' : 'a';
+
+    return (char)(((a * (ch - base) + b) % 26) + base);
+
+    }
+
+     else
+
+    {
+
+     return ch;
+
+    }
+
+}
+
+int main() 
+{
+
+ char message[] = "nhungiot";
+
+ char encrypted[100];
+
+
+ for (int i = 0; i < strlen(message); i++) 
+ {
+
+ encrypted[i] = encryptChar(message[i]);
+
+ }
+
+ encrypted[strlen(message)] = '\0';
+
+ printf("Original message: %s\n", message);
+
+ printf("Encrypted message: %s\n", encrypted);
+
+}
